5f1bcd6607db5798642de473726e80ae4298d563
Ghali Boucetta <patate.du.609@gmail.com>
2018-11-07 17:00:01 +0000

    changement t2.cpp

--- t2.cpp
+++ t2.cpp
@@ -13,20 +13,20 @@ struct etape {
     int destination;
 };
 
-lieu * foundLieu(std::vector<lieu *> * lieux, int id) {
-	for(size_t i = 0; i < lieux->size(); i++) {
-		if(lieux->at(i)->id == id) return lieux->at(i);
+int decalage(std::vector<lieu> const& lieux, int id) {
+	for(size_t i = 0; i < lieux.size(); i++) {
+		if(lieux.at(i).id == id) return lieux.at(i).decalage;
 	}
 }
 
-void meli_melo_temporel0(std::vector<lieu *> * lieux, int n, std::vector<etape *> * etapes, int v) {
+void meli_melo_temporel0(std::vector<lieu> const& lieux, int n, std::vector<etape> const& etapes, int v) {
     int jetlag, haruhi = 0, joseph = 0, lH, lJ;
     for(int i = 0; i < v; i++) {
-    	if(1 == etapes->at(i)->voyageur) if(haruhi != etapes->at(i)->destination) haruhi = etapes->at(i)->destination;
-    	if(2 == etapes->at(i)->voyageur) if(joseph != etapes->at(i)->destination) joseph = etapes->at(i)->destination;
+    	if(1 == etapes.at(i).voyageur) if(haruhi != etapes.at(i).destination) haruhi = etapes.at(i).destination;
+    	if(2 == etapes.at(i).voyageur) if(joseph != etapes.at(i).destination) joseph = etapes.at(i).destination;
     	
-    	lJ = foundLieu(lieux, joseph)->decalage;
-    	lH = foundLieu(lieux, haruhi)->decalage;
+    	lJ = decalage(lieux, joseph);
+    	lH = decalage(lieux, haruhi);
     	
     	jetlag = std::abs(lJ) + std::abs(lH);
     	
@@ -37,24 +37,24 @@ void meli_melo_temporel0(std::vector<lieu *> * lieux, int n, std::vector<etape *
 int main() {
     int destination, voyageur, v, decalage, id, n;
     std::cin >> n;
-    std::vector<lieu *> * liste_p = new std::vector<lieu *>( n );
+    std::vector<lieu> liste_p = std::vector<lieu>( n );
     for (int i = 0; i < n; i++)
     {
         std::cin >> id >> decalage;
-        lieu * lieux_i = new lieu();
-            lieux_i->id = id;
-            lieux_i->decalage = decalage;
-        liste_p->at(i) = lieux_i;
+        lieu lieux_i = lieu();
+            lieux_i.id = id;
+            lieux_i.decalage = decalage;
+        liste_p.at(i) = lieux_i;
     }
     std::cin >> v;
-    std::vector<etape *> * liste_e = new std::vector<etape *>( v );
+    std::vector<etape> liste_e = std::vector<etape>( v );
     for (int j = 0; j < v; j++)
     {
         std::cin >> voyageur >> destination;
-        etape * etape_i = new etape();
-            etape_i->voyageur = voyageur;
-            etape_i->destination = destination;
-        liste_e->at(j) = etape_i;
+        etape etape_i = etape();
+            etape_i.voyageur = voyageur;
+            etape_i.destination = destination;
+        liste_e.at(j) = etape_i;
     }
     meli_melo_temporel0(liste_p, n, liste_e, v);
     
